ebb738beedcba1427a57f08a890298d2445671cb
Rahul kumar <rkdhakad8290@gmail.com>
2023-09-27 12:00:50 +0530

    newupdate

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -9,7 +9,8 @@ public class F1 extends Car {
 
     public void accelerate(int rate){
          //set the value of new speed by using currentSpeed and rate
-       int newSpeed= getCurrentSpeed()+rate;
+        int newSpeed=0;
+        newSpeed= getCurrentSpeed()+rate;
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
